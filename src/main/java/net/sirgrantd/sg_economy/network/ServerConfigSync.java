84154ab44d97c76dd45bcaf2ec800b892809cb9e
G-Arantd <grantd.ctt@gmail.com>
2026-02-10 12:24:33 -0300

    Update ServerConfigSync.java

--- src/main/java/net/sirgrantd/sg_economy/network/ServerConfigSync.java
+++ src/main/java/net/sirgrantd/sg_economy/network/ServerConfigSync.java
@@ -6,18 +6,19 @@ import net.sirgrantd.sg_economy.config.ServerConfig;
 import net.sirgrantd.sg_economy.network.payload.SyncServerConfigS2C;
 
 public final class ServerConfigSync {
-    private ServerConfigSync() {}
+    private ServerConfigSync() {
+    }
 
     public static void syncToAllPlayersNextTick() {
-        
+
         SGEconomyMod.queueServerWork(1, () -> {
-            SGEconomyMod.LOGGER.info("Syncing ServerConfig to all players: decimalCurrency={}", ServerConfig.isDecimalCurrency);
+            SGEconomyMod.LOGGER.info("Syncing ServerConfig to all players: decimalCurrency={}",
+                    ServerConfig.isDecimalCurrency);
 
             PacketDistributor.sendToAllPlayers(new SyncServerConfigS2C(
-                ServerConfig.isDecimalCurrency,
-                ServerConfig.percentageCoinsSaveOnDeath,
-                ServerConfig.isActivePayCommand
-            ));
+                    ServerConfig.isDecimalCurrency,
+                    ServerConfig.percentageCoinsSaveOnDeath,
+                    ServerConfig.isActivePayCommand));
         });
     }
 }
